09e55cddb3242bd1ade99784439bf9e22c9e7090
Claire Trainor <trainorcn@g.cofc.edu>
2024-04-24 15:19:11 -0400

    created setlist and setinput

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -5,7 +5,19 @@ function App() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
 
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    }
 
+    //add todo to the list
+    setList([...list, newTodo]);
+
+
+    //clear input box after
+    setInput("");
+  };
 
   return (
     <div>
@@ -14,9 +26,9 @@ function App() {
       value={input} 
       onChange={(e) => setInput(e.target.value)}/>
 
-      <button>Add Something To Get Done!</button>
+      <button onClick={() => addTodo(input)}>Add Something To Get Done!</button>
     </div>
-  )
+  );
 
 }
 
