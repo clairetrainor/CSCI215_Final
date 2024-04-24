9b4eb81a5af44c80cbfa73cc3d5a1397f9dd5b93
Claire Trainor <trainorcn@g.cofc.edu>
2024-04-24 18:05:17 -0400

    attempting to revive files

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -5,18 +5,39 @@ function App() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
 
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    }
 
+    //add todo to the list
+    setList([...list, newTodo]);
+
+
+    //clear input box after
+    setInput("");
+  };
 
   return (
     <div>
       <h1>Todo List</h1>
       <input type="text" 
-      value={input} 
-      onChange={(e) => setInput(e.target.value)}/>
+      value={input}
+      onChange={(e) => setInput(e.target.value)}></input>
 
-      <button>Add Something To Get Done!</button>
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button>&times;</button>
+          </li>
+        ))}
+
+      </ul>
     </div>
-  )
+  );
 
 }
 
