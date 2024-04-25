64af0838bb02c750992956b145b49a20efcb3c2d
Claire Trainor <trainorcn@g.cofc.edu>
2024-04-25 10:12:44 -0400

    import usestate and react, created cosntants to use and addTodo

--- my-todos/src/App.js
+++ my-todos/src/App.js
@@ -1,3 +1,19 @@
 import React, {useState} from 'react'
 
-function App() {}
+function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    }
+    //add todo to the list
+    setList([...list, newTodo]);
+
+    //clear input box after entering your thing 'todo'
+    setInput("");
+  };
+}
